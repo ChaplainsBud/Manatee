fe78a4c33deda27d107f545cf98a130c38fa3fff
Matt Leroe <matthew.robert.leroe@gmail.com>
2023-06-30 14:22:01 -0400

    ResourcesView UI mockup

--- manatee_app_v0/README.md
+++ manatee_app_v0/README.md
@@ -1,6 +1,12 @@
 
 Features
 
+0. Log In. 
+
+a. Register
+
+b. Authentication
+
 1. Appointment booking system. 
 
 2. Search Engine for Matches
--- manatee_app_v0/ResourcesView.swift
+++ manatee_app_v0/ResourcesView.swift
@@ -8,31 +8,32 @@
 import SwiftUI
 
 struct ResourcesView: View {
+    let resources: [Resource] = [
+        Resource(name: "Holy Bible", url: "https://www.biblegateway.com/"),
+        Resource(name: "39 Articles Confession", url: "https://www.anglicanism.info/thirty-nine-articles-of-religion"),
+        Resource(name: "To Be a Christian Catechism", url: "https://anglicanchurch.net/wp-content/uploads/2020/06/To-Be-a-Christian.pdf"),
+        Resource(name: "The Book of Common Prayer, 2019", url: "https://bcp2019.anglicanchurch.net/")
+    ]
+    
     var body: some View {
-        VStack {
-            Text("Manatee Mentee App")
-                .font(.largeTitle)
-                .padding()
-            
-            Spacer()
-            
-            Image("manatee")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            
-            Spacer()
-            
-            Text("Enjoy Your Walk")
-                .font(.title)
-                .padding()
+        NavigationView {
+            List(resources, id: \.name) { resource in
+                Link(destination: URL(string: resource.url)!) {
+                    Text(resource.name)
+                }
+            }
+            .navigationTitle("Resources")
         }
+    }
+    
+    struct Resource {
+        let name: String
+        let url: String
     }
 }
 
-struct Resources_Previews: PreviewProvider {
+struct ResourcesView_Previews: PreviewProvider {
     static var previews: some View {
         ResourcesView()
     }
 }
-
