c38289ed11185a8e4a38ccd9acb5adde1a383fec
Matt Leroe <matthew.robert.leroe@gmail.com>
2023-06-30 13:56:15 -0400

    MatchView UI created. RegisterView updated.

--- manatee_app_v0/MatchView.swift
+++ manatee_app_v0/MatchView.swift
@@ -8,24 +8,55 @@
 import SwiftUI
 
 struct MatchView: View {
+    @State private var selectedRole: String = "Mentor"
+    @State private var selectedAge: Int = 18
+    @State private var enteredInterests: String = ""
+
+    let roles = ["Mentor", "Mentee", "Peer"]
+    let ages = Array(1...99)
+
     var body: some View {
-        VStack {
-            Text("Manatee Mentee App")
-                .font(.largeTitle)
-                .padding()
-            
-            Spacer()
-            
-            Image("manatee")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            
-            Spacer()
-            
-            Text("Enjoy Your Walk")
-                .font(.title)
-                .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    Picker("Role", selection: $selectedRole) {
+                        ForEach(roles, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    Picker("Age", selection: $selectedAge) {
+                        ForEach(ages, id: \.self) {
+                            Text("\($0)").tag($0)
+                        }
+                    }
+                    
+                    // Textfield for entering interests
+                    TextField("Enter Interests", text: $enteredInterests)
+                }
+                
+                Button(action: {
+                    print("Finding matches with Role: \(selectedRole), Age: \(selectedAge), Interests: \(enteredInterests)")
+                }) {
+                    Text("Submit")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    print("Finding random match...")
+                }) {
+                    Text("Random Match")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 10)
+            }
+            .navigationTitle("Match View")
         }
     }
 }
@@ -35,3 +66,4 @@ struct MatchView_Previews: PreviewProvider {
         MatchView()
     }
 }
+
--- manatee_app_v0/RegisterView.swift
+++ manatee_app_v0/RegisterView.swift
@@ -13,64 +13,67 @@ struct RegisterView: View {
     @State private var age = 18
     @State private var role = "Mentor"
     
+    @State private var newInterest = ""
+    @State private var interests: [String] = []
+
     let roles = ["Mentor", "Mentee", "Peer"]
 
     var body: some View {
-        
-//                        Color.black.edgesIgnoringSafeArea(.all) // Use color suitable for dark mode
-
         NavigationView {
-
-            VStack {
-//                Color.black.edgesIgnoringSafeArea(.all) // Use color suitable for dark mode
-
-                Form {
-                    
-                    Section {
-                        TextField("First Name", text: $firstName)
-                        TextField("Last Name", text: $lastName)
-                        Picker("Role", selection: $role) {
-                            ForEach(roles, id: \.self) {
-                                Text($0)
-                            }
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    Picker("Role", selection: $role) {
+                        ForEach(roles, id: \.self) {
+                            Text($0)
                         }
-                        Picker("Age", selection: $age) {
-                            ForEach(1..<100) { age in
-                                Text("\(age)").tag(age)
-                            }
+                    }
+                    Picker("Age", selection: $age) {
+                        ForEach(1..<100) { age in
+                            Text("\(age)").tag(age)
                         }
                     }
                     
-                    Section {
-                        Button("Submit") {
-                            if firstName.isEmpty || lastName.isEmpty || role.isEmpty {
-                                // Show alert if form is incomplete
-                                showAlert = true
-                            } else {
-                                // Handle the form submission
-                                //                            print("Form Submitted")
-                                print("Form Submitted:\nFirst Name: \(firstName)\nLast Name: \(lastName)\nAge: \(age)\nRole: \(role)")
-                                
+                    // Added section for interests
+                    VStack(alignment: .leading) {
+                        TextField("Add an interest", text: $newInterest)
+                        Button(action: {
+                            if !newInterest.isEmpty {
+                                interests.append(newInterest)
+                                newInterest = ""
                             }
+                        }) {
+                            Text("Add Interest")
+                        }
+                        ForEach(interests, id: \.self) { interest in
+                            Text(interest)
                         }
                     }
                 }
-                
-                .navigationTitle("User Info")
-                
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Missing Information"), message: Text("Please fill out all fields."), dismissButton: .default(Text("OK")))
+
+                Section {
+                    Button("Submit") {
+                        if firstName.isEmpty || lastName.isEmpty || role.isEmpty {
+                            // Show alert if form is incomplete
+                            showAlert = true
+                        } else {
+                            // Handle the form submission
+                            print("Form Submitted:\nFirst Name: \(firstName)\nLast Name: \(lastName)\nAge: \(age)\nRole: \(role)\nInterests: \(interests.joined(separator: ", "))")
+                        }
+                    }
                 }
-                
-                
             }
-            
+            .navigationTitle("User Info")
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Missing Information"), message: Text("Please fill out all fields."), dismissButton: .default(Text("OK")))
+            }
         }
-
     }
     
     @State private var showAlert = false
 }
+
 
 struct Register_Previews: PreviewProvider {
     static var previews: some View {
